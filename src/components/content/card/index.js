2b44c7daa686b4e95627fe8eb5ccdcbb2b675a61
Prosik <sprosimyak@gmail.com>
2022-11-16 14:34:48 +0200

    save progres

--- src/components/content/card/index.js
+++ src/components/content/card/index.js
@@ -2,17 +2,36 @@ import React from "react";
 import './style.css';
 
 function Card (props) {
+ const classNames = [
+  'card'
+ ];
+ // if (props.isVisible == false) {
+ if (!props.isVisible) {
+  classNames.push('hide');
+ }
  return (
-  <div className="card">
-    <img src={props.image} alt=""/>
-    
-    
-   <p id="text1">{props.text1}</p>
-   <p id="text2">{props.text2}</p>
-   <p id="text3">{props.text3}</p>
-   
-   
-  
+  <div className={classNames.join(' ')}>
+   <div
+    onClick={props.setSelectedCard}
+    className="card-image"
+    style={{
+     backgroundImage: `url(${props.image})`
+    }}></div>
+   <div
+    onClick={props.setSelectedCard}
+    className="card-title"
+   >{props.title}</div>
+   <div className="card-description">{props.description}</div>
+   <div className="card-date-section">
+    {props.date} |
+    <span
+     onClick={() => props.setTag(props.tag)}
+    >
+     {props.tag}
+    </span>
+
+
+   </div>
   </div>
  );
 }
--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,38 +1,34 @@
 import React from "react";
 import './style.css';
+import facebook from './imgs/5e4b1a8c95e491addf265a8e_social-03.svg';
+import instagram from './imgs/5e4b1a8c95e491834e265b00_social-07.svg';
+import twitter from './imgs/5e4b1a8c95e491586a265ad4_social-18.svg';
+import linkedin from './imgs/5e4b1a8c95e4914f3a265ae5_social-09.svg';
+import email from './imgs/5e4b1a8c95e4912fad265ab9_social-30.svg';
 
 function Navigation () {
  return (
   <nav>
-   
-   <p id="t1">Milton</p>
+   <div className="menu-title">Milton</div>
    <hr />
-   <p id="t4">
-   Denali is a simple responsive blog template. Easily add new posts using the Editor or change layout and design using the Designer.
-   </p>
-   <hr />
-   <p id="t2">home</p>
-   <p id="t2">about</p>
-   <p id="t2">contact</p>
-   <hr />
-   <div class="icons">
-    <div> 
-        <img src="https://assets.website-files.com/5e4b1a8c95e491ce78265a6f/5e4b1a8c95e491addf265a8e_social-03.svg"></img>
-    </div>
-    <div>
-        <img src="	https://assets.website-files.com/5e4b1a8c95e491ce78265a6f/5e4b1a8c95e491834e265b00_social-07.svg "></img>
-    </div>
-    <div>
-    <img src=" 	https://assets.website-files.com/5e4b1a8c95e491ce78265a6f/5e4b1a8c95e491586a265ad4_social-18.svg"></img>
-    </div>
-    <div>
-    <img src="	https://assets.website-files.com/5e4b1a8c95e491ce78265a6f/5e4b1a8c95e4914f3a265ae5_social-09.svg "></img>
-    </div>
-    <div>
-    <img src=" 	https://assets.website-files.com/5e4b1a8c95e491ce78265a6f/5e4b1a8c95e4912fad265ab9_social-30.svg"></img>
-    </div>
+   <div className="menu-text-block">
+    Denali is a simple responsive blog template. Easily add new posts using the Editor or change layout and design using the Designer.
    </div>
-   <p id="t3">BUILT WITH WEBFLOW</p>
+   <hr />
+   <ul className="menu">
+    <li>home</li>
+    <li>about</li>
+    <li>contact</li>
+   </ul>
+   <hr />
+   <div className="media-icons">
+    <img src={facebook} alt='' />
+    <img src={instagram} alt='' />
+    <img src={twitter} alt='' />
+    <img src={linkedin} alt='' />
+    <img src={email} alt='' />
+   </div>
+   <span>Built with Webflow</span>
   </nav>
  );
 }
