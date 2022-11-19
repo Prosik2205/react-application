f3ee210efaf05d5e8e9ad767eb45e9693d2d6cce
Prosik <sprosimyak@gmail.com>
2022-11-19 14:29:51 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Menu from './components/navigation';
 import MyCarousel from './components/carousel';
+import  Main from './components/Main';
 
 function App () {
   return (
     <div className="App">
       <Menu />
       <MyCarousel />
+      <Main />
     </div>
   );
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,2 +1,22 @@
 import React, { useState } from "react";
 import './style.css';
+import './img/100.gif'
+function Menu(){
+    return(
+        <div className="grid">
+            <div className="con">
+                <div className="black">
+                    {/* <img src="100/gif">  </img> */}
+                </div>
+                <p>Headline Text</p>
+                <p>Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.</p>
+                Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.
+                <p>Read More »</p>
+
+
+            </div>
+        </div>
+
+    );
+}
+export default Menu;
