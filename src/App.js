3fc1d2e48201aad5333047039c7945335c3cd7e3
Prosik <sprosimyak@gmail.com>
2022-11-19 02:35:57 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import Menu from './components/navigation';
+import MyCarousel from './components/carousel';
 
 function App () {
   return (
     <div className="App">
-    
+      <Menu />
+      <MyCarousel />
     </div>
   );
 }
