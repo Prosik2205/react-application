d6d39c2fb94d57418c13905ec46c585049483c17
Prosik <sprosimyak@gmail.com>
2022-11-19 22:09:14 +0200

    save

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,20 +1,81 @@
 import React, { useState } from "react";
 import './style.css';
-import './img/100.gif'
+import kut from './img/100.gif'
 function Menu(){
     return(
+        <div className="container">
         <div className="grid">
+
+            {/* <div className="black">
+                    {/* <img src={kut} alt=''>  </img> 
+                </div> */}
+
             <div className="con">
-                <div className="black">
-                    {/* <img src="100/gif">  </img> */}
-                </div>
-                <p>Headline Text</p>
-                <p>Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.</p>
-                Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.
-                <p>Read More »</p>
-
-
+                <h2>Headline Text</h2>
+                <p className="text">Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.</p>
+                <p className="text"> Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                <p >Read More »</p>
             </div>
+            <div className="con">
+                <h2>Headline Text</h2>
+                <p className="text">Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.</p>
+                <p className="text"> Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                <p >Read More »</p>
+            </div>
+            <div className="con">
+                <h2>Headline Text</h2>
+                <p className="text">Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.</p>
+                <p className="text"> Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                <p >Read More »</p>
+            </div>
+            <div className="con">
+                <h2>Headline Text</h2>
+                <p className="text">Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.</p>
+                <p className="text"> Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                <p >Read More »</p>
+            </div>
+        </div>
+        <div className="hr"> </div>
+        <div className="grid">
+            
+        <div className="con">
+                <h2>Headline Text</h2>
+                <p className="text2">Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                <p className="text2">Vestibulumaccumsan egestibulum eu justo convallis augue estas aenean elit intesque sed. Facilispede estibulum nulla orna nisl velit elit ac aliquat non tincidunt.</p>
+                <p className="text2">Namjusto cras urna urnaretra lor urna neque sed quis orci nulla. Laoremut vitae doloreet condimentumst phasellentes dolor ut a ipsum id consectetus.</p>
+            </div>
+            <div className="con">
+                <h2>Headline Text</h2>
+                <p className="text2">Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                <p className="text2">Vestibulumaccumsan egestibulum eu justo convallis augue estas aenean elit intesque sed. Facilispede estibulum nulla orna nisl velit elit ac aliquat non tincidunt.</p>
+                <p className="text2">Namjusto cras urna urnaretra lor urna neque sed quis orci nulla. Laoremut vitae doloreet condimentumst phasellentes dolor ut a ipsum id consectetus.</p>
+            </div>
+            <div className="con">
+                <h2>Headline Text</h2>
+                <p className="text2">Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                <p className="text2">Vestibulumaccumsan egestibulum eu justo convallis augue estas aenean elit intesque sed. Facilispede estibulum nulla orna nisl velit elit ac aliquat non tincidunt.</p>
+                <p className="text2">Namjusto cras urna urnaretra lor urna neque sed quis orci nulla. Laoremut vitae doloreet condimentumst phasellentes dolor ut a ipsum id consectetus.</p>
+            </div>
+            <div className="con">
+                <h2>Headline Text</h2>
+                <p className="text2">Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                <p className="text2">Vestibulumaccumsan egestibulum eu justo convallis augue estas aenean elit intesque sed. Facilispede estibulum nulla orna nisl velit elit ac aliquat non tincidunt.</p>
+                <p className="text2">Namjusto cras urna urnaretra lor urna neque sed quis orci nulla. Laoremut vitae doloreet condimentumst phasellentes dolor ut a ipsum id consectetus.</p>
+            </div>
+            </div>
+            <footer>
+                <div className="ftr">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+
+
+                </div>
+            </footer>
+
+
+
         </div>
 
     );
