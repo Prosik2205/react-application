5f2f32afdb7447b35dd7a149508a48864804b1a2
Prosik <sprosimyak@gmail.com>
2022-11-20 16:58:12 +0200

    save

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Menu from './components/navigation';
 import MyCarousel from './components/carousel';
 import  Main from './components/Main';
-
+//npm install react-material-ui-carousel
 function App () {
   return (
     <div className="App">
